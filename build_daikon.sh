b3f6e2eaec883ecdd3e043a03ccc67d08c02ed87
Michael Ernst <mernst@cs.washington.edu>
2019-12-31 17:09:10 -0800

    Add debugging

--- build_daikon.sh
+++ build_daikon.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 DAIKON_TARBALL=$1
 DAIKON_PARENT_DIR=`dirname $1`
 
-echo JAVA_HOME: ${JAVA_HOME:?"Building Daikon requires the JAVA_HOME environment variable to be set."}
+echo build_daikon.sh: DAIKON_TARBALL=$DAIKON_TARBALL
+echo build_daikon.sh: DAIKON_PARENT_DIR=$DAIKON_PARENT_DIR
+echo build_daikon.sh: JAVA_HOME=${JAVA_HOME}
 
 pushd $DAIKON_PARENT_DIR
     DAIKON_SRC_DIR=`tar -tzf ${DAIKON_TARBALL} | head -1 | cut -f1 -d"/"`
+    echo build_daikon.sh: DAIKON_SRC_DIR=${DAIKON_SRC_DIR}
     tar xzf $DAIKON_TARBALL
     mv $DAIKON_SRC_DIR daikon-src
     pushd daikon-src
